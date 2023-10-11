460925a991a108a782ed8c45177492437623583a
Max Z <maxsis100@gmail.com>
2023-10-11 15:03:27 +0800

    update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,15 +6,16 @@
 const hre = require("hardhat");
 
 async function main() {
-  const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
-  const nftMarket = await NFTMarket.deploy();
-  await nftMarket.deployed();
-  console.log("nftMarket deployed to:", nftMarket.address);
+  
+  const DutchAuction = await hre.ethers.getContractFactory("DutchAuction");
+  const dutchAuction = await DutchAuction.deploy();
+  await dutchAuction.deployed();
+  console.log("dutchAuction deployed to:", dutchAuction.address);
 
-  const NFT = await hre.ethers.getContractFactory("NFT");
-  const nft = await NFT.deploy(nftMarket.address);
-  await nft.deployed();
-  console.log("nft deployed to:", nft.address);
+  const CodePanther = await hre.ethers.getContractFactory("CodePanther");
+  const codePanther = await CodePanther.deploy();
+  await codePanther.deployed();
+  console.log("codePanther deployed to:", codePanther.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
