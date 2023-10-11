3a0513aa0927bd132a4b34ac484f360e8c37134b
Max Z <maxsis100@gmail.com>
2023-10-11 16:01:16 +0800

    add test cases

--- test/test.js
+++ test/test.js
@@ -65,3 +65,46 @@ describe("NFTMarket", function() {
     console.log('myNfts:', myNfts);
   });
 });
+
+describe("DutchAuction", function() {
+  beforeEach(async function() {
+    
+    const DutchAuction = await ethers.getContractFactory("DutchAuction");
+    const dutchAuction = await DutchAuction.deploy();
+    await dutchAuction.deployed()
+    const dutchAuctionAddress = dutchAuction.address; 
+  });
+
+  it("Every bid emits an event with transaction details", async function() {
+    
+  });
+
+  it("Auction should close when all tokens are reserved", async function() {
+    // New users cannot bid after all tokens are reserved
+
+  });
+
+  it("Auction should close when reservation price === current price", async function() {
+
+  });
+
+  it("Auction should close when time is up", async function() {
+
+  });
+
+  it("Successful bidders should receive correct no. of tokens", async function() {
+
+  });
+
+  it("Remaining tokens after auction is closed should be burned", async function() {
+
+  });
+
+  it("Owners should be able to withdraw Ether from auction", async function() {
+
+  });
+
+  // Reentry attack
+
+  // Submarine case
+});
